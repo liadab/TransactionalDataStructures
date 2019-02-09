72522372e81f71d5d07a2c038bf0e36baf0882cc
almog <almogfrei@gmail.com>
2019-02-09 12:06:33 +0200

    not working LNode

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "nodes/LNode.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     size_t a;
     a = 5;
+    LNode <int, int> n;
     std::cout << a << std::endl;
     return 0;
 }
--- nodes/LNode.h
+++ nodes/LNode.h
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <atomic>
 #include <memory>
+#include <cassert>
 
 template <typename key_t, typename val_t>
 class LNode {
@@ -15,42 +16,136 @@ private:
             return false;
         }
         uint64_t locked = l | LOCK_MASK;
-        return m_version_mask.compareAndSet(l, locked);
+        return m_version_mask.compare_exchange_strong(l, locked);
     }
 
     void unlock() {
-        uint64_t l = m_version_mask.get();
+        uint64_t l = m_version_mask;
         assert ((l & LOCK_MASK) != 0 && "unlocking a node that is not locked");
         uint64_t  unlocked = l & (~LOCK_MASK);
-        bool ret = m_version_mask.compareAndSet(l, unlocked);
-        assert (ret);
+        bool ret = m_version_mask.compare_exchange_strong(l, unlocked);
+        assert (ret && "compare_exchange_strong in unlock failed");
     }
 
     bool isLocked() {
-        uint64_t l = m_version_mask.get();
-        return (l & lockMask) != 0;
+        uint64_t l = m_version_mask;
+        return (l & LOCK_MASK) != 0;
     }
 
     bool isDeleted() {
-        uint64_t l = m_version_mask.get();
-        return (l & deleteMask) != 0;
+        uint64_t l = m_version_mask;
+        return (l & DELETE_MASK) != 0;
     }
 
     void setDeleted(bool value) {
-        uint64_t l = m_version_mask.get();
-        assert ((l & lockMask) != 0);
+        uint64_t l = m_version_mask;
+        assert ((l & LOCK_MASK) != 0 && "setDeleted on unlocked node");
         if (value) {
-            l |= deleteMask;
-            m_version_mask.set(l);
+            l |= DELETE_MASK;
+            m_version_mask = l;
             return;
         }
-        l &= (~deleteMask);
-        m_version_mask.set(l);
+        l &= (~DELETE_MASK);
+        m_version_mask = l;
     }
 
     bool isLockedOrDeleted() {
-        uint64_t l = m_version_mask.get();
-        return ((l & deleteMask) != 0) || ((l & lockMask) != 0);
+        uint64_t l = m_version_mask;
+        return ((l & DELETE_MASK) != 0) || ((l & LOCK_MASK) != 0);
+    }
+
+    bool isSingleton() {
+        uint64_t l = m_version_mask;
+        return (l & SINGLETON_MASK) != 0;
+    }
+
+    void setSingleton(bool value) {
+        uint64_t l = m_version_mask;
+        assert ((l & LOCK_MASK) != 0 && "setSingleton on unlocked node");
+        if (value) {
+            l |= SINGLETON_MASK;
+            m_version_mask = l;
+            return;
+        }
+        l &= (~SINGLETON_MASK);
+        m_version_mask = l;
+    }
+
+    void setSingletonNoLockAssert(bool value) {
+        uint64_t l = m_version_mask;
+        if (value) {
+            l |= SINGLETON_MASK;
+            m_version_mask = l;
+            return;
+        }
+        l &= (~SINGLETON_MASK);
+        m_version_mask = l;
+    }
+
+    uint64_t getVersion() {
+        return (m_version_mask & (~VERSIONNEG_MASK));
+    }
+
+    void setVersion(uint64_t version) {
+        uint64_t l = m_version_mask;
+        assert ((l & LOCK_MASK) != 0);
+        l &= VERSIONNEG_MASK;
+        l |= (version & (~VERSIONNEG_MASK));
+        m_version_mask = l;
+    }
+
+    bool isSameVersionAndSingleton(uint64_t version) {
+        uint64_t l = m_version_mask;
+        if ((l & SINGLETON_MASK) != 0) {
+            l &= (~VERSIONNEG_MASK);
+            return l == version;
+        }
+        return false;
+    }
+
+    void setVersionAndSingleton(uint64_t version, bool value) {
+        uint64_t l = m_version_mask;
+        assert ((l & LOCK_MASK) != 0);
+        l &= VERSIONNEG_MASK;
+        l |= (version & (~VERSIONNEG_MASK));
+        if (value) {
+            l |= SINGLETON_MASK;
+            m_version_mask = l;
+            return;
+        }
+        l &= (~SINGLETON_MASK);
+        m_version_mask = l;
+    }
+
+    void setVersionAndSingletonNoLockAssert(uint64_t version, bool value) {
+        uint64_t l = m_version_mask;
+        l &= VERSIONNEG_MASK;
+        l |= (version & (~VERSIONNEG_MASK));
+        if (value) {
+            l |= SINGLETON_MASK;
+            m_version_mask = l;
+            return;
+        }
+        l &= (~SINGLETON_MASK);
+        m_version_mask = l;
+    }
+
+    void setVersionAndDeletedAndSingleton(uint64_t version, bool deleted, bool singleton) {
+        uint64_t l = m_version_mask;
+        assert ((l & LOCK_MASK) != 0 && "setVersionAndDeletedAndSingleton on unlocked node");
+        l &= VERSIONNEG_MASK;
+        l |= (version & (~VERSIONNEG_MASK));
+        if (singleton) {
+            l |= SINGLETON_MASK;
+        } else {
+            l &= (~SINGLETON_MASK);
+        }
+        if (deleted) {
+            l |= DELETE_MASK;
+        } else {
+            l &= (~DELETE_MASK);
+        }
+        m_version_mask = l;
     }
     
     static constexpr uint64_t LOCK_MASK = 0x1000000000000000L;
