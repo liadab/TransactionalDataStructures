773af332a8788ea0900b26a2ba6d4640fc2e5d0a
liad <liad.abenzour64@gmail.com>
2019-02-17 21:49:26 +0200

    add test for put and fixed bug

--- WriteElement.h
+++ WriteElement.h
@@ -2,6 +2,16 @@
 
 #include "nodes/LNode.h"
 
+template <typename T>
+std::ostream& operator<< (std::ostream& stream, const std::optional<T>& v) {
+    if(!v) {
+        stream << "None";
+    } else {
+        stream << *v;
+    }
+    return stream;
+}
+
 template <typename key_t, typename val_t>
 class WriteElement {
 public:
@@ -11,4 +21,9 @@ public:
     node_t next;
     std::optional<val_t> val; // used when changing value with put
     bool deleted;
+
+    friend std::ostream& operator<< (std::ostream& stream, const WriteElement<key_t, val_t>& we) {
+        stream << "val: " << we.val << " d?" << we.deleted << " next: " << we.next;
+        return stream;
+    }
 };
