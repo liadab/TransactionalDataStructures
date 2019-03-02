25af998906714733093c598e0516354820c8027c
sapkode <sapirfreizeit@gmail.com>
2019-03-02 14:30:27 +0200

    added java code and python, and results to git, under 'bacnhmark'

---
+++ banchmark/src/main/LockQueue.java
@@ -0,0 +1,28 @@
+package main;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockQueue {
+
+    private ReentrantLock lock;
+    // TODO which lock to use?
+
+    LockQueue() {
+        lock = new ReentrantLock();
+    }
+
+    protected void lock() {
+        if (!lock.isHeldByCurrentThread()) {
+            lock.lock();
+        }
+    }
+
+    protected void unlock() {
+        assert (lock.isHeldByCurrentThread());
+        lock.unlock();
+    }
+
+    protected boolean tryLock() {
+        return lock.isHeldByCurrentThread() || lock.tryLock();
+    }
+
+}
---
+++ banchmark/src/main/TXLibExceptions.java
@@ -0,0 +1,14 @@
+package main;
+public class TXLibExceptions {
+
+    @SuppressWarnings("serial")
+    public class QueueIsEmptyException extends Exception {
+
+    }
+
+    @SuppressWarnings("serial")
+    public class AbortException extends RuntimeException {
+
+    }
+
+}
